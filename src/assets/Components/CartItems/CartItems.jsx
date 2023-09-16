1035aaf8e11e81560512d754dcbb6716e13f7a65
Shaon22 <shaonroy11@gmail.com>
2023-09-16 14:37:41 +0600

    added the total price feature that user can see the total price of their added courses

--- src/assets/Components/CartItems/CartItems.jsx
+++ src/assets/Components/CartItems/CartItems.jsx
@@ -5,7 +5,7 @@ import CartItem from "../CartItem/Cartitem";
 const CartItems = ({cartItems,remainingCreditHr,totalPrice,totalCreditHour}) => {
     
     return (
-        <div className='bg-[#FFFFFF] shadow-2xl p-3 rounded-xl w-1/4'>
+        <div className='bg-[#FFFFFF] shadow-2xl p-3 rounded-xl w-1/4 h-[395px]'>
              <h1 className="text-lg font-semibold text-sky-400 border-b-2 mb-4 pb-2">Credit hour remaining {remainingCreditHr} hr</h1>
              <h1 className="text-lg font-semibold text-[#1C1B1B]  pb-2">Course Name</h1>
             
@@ -15,6 +15,7 @@ const CartItems = ({cartItems,remainingCreditHr,totalPrice,totalCreditHour}) => 
             }
            </ol>
            <h1 className="text-lg font-semibold text-[#1C1B1B] border-b-2 border-t-2 py-2 my-4"  >Total credit hr: {totalCreditHour}</h1>
+           <h1 className="text-lg font-semibold text-[#1C1B1B]">Total price: $ {totalPrice}</h1>
         </div>
     );
 };
